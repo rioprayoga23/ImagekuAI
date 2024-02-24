ea481f07e42d6ed1e61ccb644faee3829ae6e3ea
Rio Prayoga <yoprayogap@gmail.com>
2024-02-24 00:04:13 -0800

    feat: auth

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div>
+      HomePage
+      <UserButton afterSignOutUrl="/" />
+    </div>
+  );
 };
 
 export default HomePage;
