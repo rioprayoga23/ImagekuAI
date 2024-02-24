88cd868ab01af91d9ceb608c36b7335f3524780b
Fahmi Fauzi <fahmi.fauzi@ifg-life.id>
2024-02-24 20:18:09 +0700

    feat: add set public metadata user

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,10 +1,11 @@
 /* eslint-disable camelcase */
-import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
+import { WebhookEvent } from "@clerk/nextjs/server";
 import { Webhook } from "svix";
 
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.action";
+import { clerkClient } from "@clerk/nextjs";
 
 export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
@@ -73,13 +74,13 @@ export async function POST(req: Request) {
     const newUser = await createUser(user);
 
     // Set public metadata
-    // if (newUser) {
-    //   await clerkClient.users.updateUserMetadata(id, {
-    //     publicMetadata: {
-    //       userId: newUser._id,
-    //     },
-    //   });
-    // }
+    if (newUser) {
+      await clerkClient.users.updateUserMetadata(id, {
+        publicMetadata: {
+          userId: newUser._id,
+        },
+      });
+    }
 
     return NextResponse.json({ message: "OK", user: newUser });
   }
